35b3ac2d7f2df230edee9d3e451e298d79060c50
Foke-Edmilson <efoj1@aluno.ifal.edu.br>
2026-04-13 15:06:29 -0300

    Add rev1.java to calculate and print sum

---
+++ rev1.java
@@ -0,0 +1,13 @@
+public class rev1 {
+    public static void main(String[] args) {
+        int z = 10;
+        int sum = 0;
+
+        while (z > 0 ) {
+            sum += z;
+            z--; 
+            System.out.println(sum);
+    }
+        System.out.println("Soma: " + sum);
+    }
+}
